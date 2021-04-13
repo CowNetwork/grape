dcac8ee8e67c7b202ed3c45b08c10fad7704d4af
Tobias B <germansuperioz@yahoo.de>
2021-04-13 11:28:30 +0200

    Add Spigot module

---
+++ grape-spigot/src/main/java/network/cow/grape/spigot/GrapePlugin.java
@@ -0,0 +1,13 @@
+package network.cow.grape.spigot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * We don't need anything here. We just want to shade the grape components
+ * into our plugin.
+ *
+ * @author Tobias Büser
+ */
+public class GrapePlugin extends JavaPlugin {
+
+}
